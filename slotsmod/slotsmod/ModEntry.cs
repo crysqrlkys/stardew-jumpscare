d257a8559d3c78d7a4b60dc54f12e7c98918cb9e
Kanstantsin <whykonstantin@gmail.com>
2025-07-23 22:35:49 +0300

    Initial Setup

--- slotsmod/slotsmod/ModEntry.cs
+++ slotsmod/slotsmod/ModEntry.cs
@@ -16,6 +16,7 @@ namespace slotsmod
 
         private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
         {
+            //ignore if not in game
             if (!Context.IsWorldReady)
                 return;
 
